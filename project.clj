e2f5bbb42645b01fc0b0b3e57f7dadadd3abc7d1
konrad-garus <konrad.garus@gmail.com>
2011-10-08 18:46:13 +0200

    Added github link to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject yaclot "0.1.2-SNAPSHOT"
   :description "Yet another Clojure conversion and record transformation library."
+  :url "https://github.com/konrad-garus/yaclot"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
